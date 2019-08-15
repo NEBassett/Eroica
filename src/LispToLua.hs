fcadd2380186b5e24a501afa9902a1a357fb892f
nicklausEB <nicklaus.e.b@gmail.com>
2019-08-14 23:28:31 -0400

    fixed some error messages

--- src/LispToLua.hs
+++ src/LispToLua.hs
@@ -33,7 +33,7 @@ toLua (L.List ((L.Symbol "if"):cond:(L.List body):rest)) = do
       return $ If x y z w
     [] -> return $ If x y (Bool False) []
     m -> throwError $
-         L.TypeMismatch "Expected either empty list or else condition and body for if call, got: " (L.List m)
+         L.TypeMismatch "either empty list or else condition and body for if call" (L.List m)
 toLua (L.List [L.Symbol "while", cond, (L.List body)]) = do
   x <- toLua cond
   y <- mapM toLua body
@@ -61,7 +61,7 @@ toLua (L.List [L.Symbol "gen-for", (L.List vars), iter, (L.List body)]) = do
 toLua (L.List [L.Symbol "array", L.List args])  = (mapM toLua args) >>= (\x -> return $ Array x)
 toLua (L.List [L.Symbol "map", L.List args]) = (mapM toPairs args) >>= (\x -> return $ Map x)
   where toPairs (L.List pairs@[field, value]) = (mapM toLua pairs)
-        toPairs m = throwError $ L.TypeMismatch "Expected pair of values in the form (field value) in map call, got: " m
+        toPairs m = throwError $ L.TypeMismatch "pair of values in the form (field value) in map call" m
 toLua (L.List (L.Symbol "lambda" : L.List params : body)) = (mapM toLua body) >>= (\x -> return $ Func (fmap show params) Nothing x) 
 toLua (L.List (L.Symbol "lambda" : L.DottedList params varargs : body)) = (mapM toLua body) >>= (\x -> return $ Func (fmap show params) (Just $ show varargs) x)
 toLua (L.List [L.Symbol "index", tab, field]) = (toLua tab) >>= (\x -> (toLua field) >>= (\y -> return $ Index x  y))
